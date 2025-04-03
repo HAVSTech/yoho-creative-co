cf8bda1a49a3efe3e54dc07eb5c22c20a8893fdb
svharisaravanan <sharisaravanan@gmail.com>
2025-04-03 21:28:02 +0530

    Final

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // to get current year
 function getYear() {
     var currentDate = new Date();
@@ -26,4 +27,34 @@ $('.owl-carousel').owlCarousel({
             items: 6
         }
     }
+=======
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+// owl carousel 
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+>>>>>>> e07c443 (Intial commit)
 })
